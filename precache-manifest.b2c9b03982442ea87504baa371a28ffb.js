617e4826005cf0a765011aea78da23bd51b0ab0d
DjorgeSilva <flanelon18@gmail.com>
2021-03-23 10:54:27 -0300

    Updates

---
+++ precache-manifest.b2c9b03982442ea87504baa371a28ffb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed913e772a20c9bdbff4da65f41dd35",
+    "url": "/personal-album-website/index.html"
+  },
+  {
+    "revision": "76057c2059ffc1543c1c",
+    "url": "/personal-album-website/static/css/main.8f059818.chunk.css"
+  },
+  {
+    "revision": "acbba2a7e52cb8bcab84",
+    "url": "/personal-album-website/static/js/2.ada05018.chunk.js"
+  },
+  {
+    "revision": "f3b3a612f6bc533d81895a5d8cc56229",
+    "url": "/personal-album-website/static/js/2.ada05018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76057c2059ffc1543c1c",
+    "url": "/personal-album-website/static/js/main.84d6afd4.chunk.js"
+  },
+  {
+    "revision": "37f148ae74c29f193b1c",
+    "url": "/personal-album-website/static/js/runtime-main.f7d72bba.js"
+  },
+  {
+    "revision": "3621c5a58b36a1fc2ed047ba8e5dddaa",
+    "url": "/personal-album-website/static/media/bg.3621c5a5.jpeg"
+  },
+  {
+    "revision": "fc568e75874dc3185a0b3b4249f66f77",
+    "url": "/personal-album-website/static/media/slider.fc568e75.jpeg"
+  },
+  {
+    "revision": "43386b3151e1c3792cc717f96b0e7713",
+    "url": "/personal-album-website/static/media/slider1.43386b31.jpeg"
+  },
+  {
+    "revision": "750df8218468b92e02195e03edade68b",
+    "url": "/personal-album-website/static/media/slider2.750df821.jpeg"
+  },
+  {
+    "revision": "6e4db0ef4939188b81b17310a3703132",
+    "url": "/personal-album-website/static/media/slider3.6e4db0ef.jpeg"
+  }
+]);
